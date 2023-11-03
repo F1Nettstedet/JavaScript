7160e3124f4f1d56c1b68cad8d4c0d5e580e7c21
Nikita Ilinykh <Mussolq@gmail.com>
2023-11-03 20:38:23 +0500

    Выполнение 2 домашнего задания

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let title = 1
-let screens = 2
-let screenPrice = 3
-let rollback = 4
-let fullPrice = 5
-let adaptive = 6
+let title = "JavaScript"
+let screens = "Простые, Сложные, Интерактивные"
+let screenPrice = 10
+let rollback = 50
+let fullPrice = 50000
+let adaptive = false
 
 alert('Hello World')
 
 console.log('Hello World')
+
+console.log(typeof(title),typeof(fullPrice),typeof(adaptive))
+
+console.log(screens.length)
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани " + "\n" + "Стоимость разработки сайта " + fullPrice +  "  рублей/ долларов/гривен/юани")
+
+console.log(screens.toLocaleLowerCase().split())
+
+console.log(fullPrice * (rollback / 100))
