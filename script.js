a0e248ad7df19f980a001de8c7f3ca0e6ee0cf4e
Nikita Ilinykh <Mussolq@gmail.com>
2023-11-06 11:53:55 +0500

    Выполнение 3 домашнего задания

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-let title = "JavaScript"
-let screens = "Простые, Сложные, Интерактивные"
-let screenPrice = 10
+let title = prompt('Как называется ваш проект?')
+let screens = prompt('Какие типы экранов нужно разработать?','Пример: Простые, Сложные, Интерактивные')
+let screenPrice = +prompt('Сколько будет стоить данная работа?')
 let rollback = 50
 let fullPrice = 50000
-let adaptive = false
+let adaptive = confirm('Нужен ли адаптив на сайте?')
+
+
+
+let service1 = confirm('Какой допольнительный тип услуги нужен Service 1?')
+let servicePrice1 = +prompt('Сколько это будет стоить?')
+let service2 = confirm('Какой допольнительный тип услуги нужен Service 2?')
+let servicePrice2 = +prompt('Сколько это будет стоить?')
+
+console.log(fullPrice = screenPrice + servicePrice1 + servicePrice2)
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice *(rollback / 100)))
+
+console.log(servicePercentPrice)
+
+if(fullPrice >  30000){
+    console.log('Даем скидку в 10%')
+}else if(fullPrice > 15000 && fullPrice <= 30000 ){
+    console.log('Даем скидку в 5%')
+}else if(fullPrice < 15000 && fullPrice > 0){
+    console.log('Скидка не предусмотрена')
+}else if(fullPrice < 0 ){
+    console.log('Что-то пошло не так')
+}
+
 
 alert('Hello World')
 
