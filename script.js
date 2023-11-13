1981d59f0a5f90421c1afcf54287721af04c93c5
Nikita Ilinykh <Mussolq@gmail.com>
2023-11-13 21:18:34 +0500

    Выполнение 8 домашнего заадния

--- script.js
+++ script.js
@@ -4,65 +4,87 @@
 const appData = {
 
     title: '',
-    screens: '',
+    screens: [],
     screenPrice: 0,
     adaptive: true,
     rollback: 50,
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrice: 0,
-    service1: '',
-    service2: '',
+    services: {},
 
+    isString: function(str){ 
+        return isNaN(str)
+    },
 
     isNumber: function (num) {
         return !isNaN(parseFloat(num) && isFinite(num))
     },
 
     asking: function () {
-        appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки')
-        appData.screens = prompt('Какие типы экранов нужно разработать?', 'Пример: Простые, Сложные, Интерактивные')
+        let name = ''
+        let price = 0
+
         do {
-            appData.screenPrice = +prompt('Сколько будет стоить данная работа?')
+            appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки')
+        } while(this.isNumber(this.title))
+
+        for (let i = 0; i < 2; i++) {
+            do {
+                name = prompt('Какие типы экранов нужно разработать?', 'Пример: Простые, Сложные, Интерактивные')
+            } while(this.isNumber(name))
+
+            do {
+                price = prompt('Сколько будет стоить данная работа?')
+            }
+            while (!appData.isNumber(price))
+
+            appData.screens.push({ id: i, name: name, price: price})
+
         }
-        while (!appData.isNumber)(appData.screenPrice)
+
+        for (let i = 0; i < 2; i++) {
+            let name = ''
+            let price = 0
+
+            do {
+                name = prompt('Какой допольнительный тип услуги нужен ?')
+            } while(this.isNumber(name))
+            
+
+            do {
+                price = prompt('Сколько это будет стоить?')
+            } while (!appData.isNumber(price))
+
+            appData.services[name + i] = +price   // по-другому что-то пока не дошло как  сделать так,чтобы при передаче в объект одинакового ключа не было перезаписи =(
+        }
 
         appData.adaptive = confirm('Нужен ли адаптив на сайте?')
     },
 
-    getAllServicePrices: function () {
-        let sum
-        let saveNum = 0
-        for (let i = 0; i < 2; i++) {
-            if (i === 0) {
-                appData.service1 = prompt('Какой допольнительный тип услуги нужен Service 1?')
-            } else if (i === 1) {
-                appData.service2 = prompt('Какой допольнительный тип услуги нужен Service 2?')
-
-            }
-
-            sum = +prompt('Сколько это будет стоить?')
-
-            while (!appData.isNumber(sum)) {
-                sum = +prompt('Сколько это будет стоить?')
-            }
-
-            saveNum += sum
+    addPrices: function () {
+        for (let screen of appData.screens) {
+            appData.screenPrice += +screen.price
         }
-        return saveNum
+
+        for (key in appData.services) {
+            appData.allServicePrices += this.services[key]
+        }
+
     },
 
+
     getFullPrice: function () {
-        return (appData.screenPrice + appData.allServicePrices)
+        appData.fullPrice = (appData.screenPrice + appData.allServicePrices)
     },
 
     getServicePercentPrices: function () {
-        return (appData.fullPrice - (appData.fullPrice * (appData.rollback / 100)))
+        appData.servicePercentPrice = (appData.fullPrice - (appData.fullPrice * (appData.rollback / 100)))
 
     },
 
     getTitle: function () {
-        return (appData.title.trim()[0].toUpperCase() + appData.title.slice(1).toLowerCase())
+        appData.title = (appData.title.trim()[0].toUpperCase() + appData.title.slice(1).toLowerCase())
     },
 
     getRollbackMessage: function (price) {
@@ -81,28 +103,32 @@ const appData = {
         }
 
     },
-    logger: function (){
+    logger: function () {
         console.log(this.fullPrice)
         console.log(this.servicePercentPrice)
-        for(key in appData){ 
-            if(typeof appData[key] == 'function'){
-                console.log('Метод: ' + key )
-            }else{ 
+        console.log(appData.screens)
+        console.log(appData.services)
+        for (key in appData) {
+            if (typeof appData[key] == 'function') {
+                console.log('Метод: ' + key)
+            } else {
                 console.log('Свойство: ' + key)
             }
         }
 
-    }, 
+
+    },
 
     start: function () {
-        this.asking()
-        appData.allServicePrices = appData.getAllServicePrices()
-        appData.fullPrice = appData.getFullPrice()
-        appData.servicePercentPrice = appData.getServicePercentPrices()
-        appData.title = appData.getTitle()
-        this.logger()
+        appData.asking()
+        appData.addPrices()
+        appData.getFullPrice()
+        appData.getServicePercentPrices()
+        appData.getTitle()
+        appData.logger()
+        
     },
-   
+
 }
 
 appData.start()
